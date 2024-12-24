74efb778906c39c4f1e5b74de93c935bea933567
Sepide Kia <sepidekia6990@gmail.com>
2024-12-24 10:58:22 +0330

    feat: add the header of layout with logo

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,8 +1,20 @@
+import Settings from "@/features/settings";
+import { Link } from "react-router";
+import Logo from "./Logo";
+
 export default function Header() {
   return (
     <header className="bg-white shadow">
-      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-gray-900">News Aggregator</h1>
+      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between">
+        <Link to="/">
+          <div className="flex items-center">
+            <Logo />
+            <h2 className="text-2xl font-bold text-gray-900">
+              News Aggregator
+            </h2>
+          </div>
+        </Link>
+        <Settings />
       </div>
     </header>
   );
