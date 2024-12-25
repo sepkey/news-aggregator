507c827a5c289a28881c11b249d29d68b73dfa54
Sepide Kia <sepidekia6990@gmail.com>
2024-12-25 15:41:39 +0330

    chore: install and config react-query, add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Layout from "./layout/inedx";
 import Home from "./pages/Home";
 import NewsDetails from "./pages/NewsDetails";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/:newsId" element={<NewsDetails />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" index element={<Home />} />
+            <Route path="/article/:newsId" element={<NewsDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
