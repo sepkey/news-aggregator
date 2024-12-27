fc4ff6989bcaac2c53c4a51409d65bda2465f2d6
Sepide Kia <sepidekia6990@gmail.com>
2024-12-27 18:29:16 +0330

    refactor: refactor the Home page into separate  custom hook and ui part

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import ErrorMessage from "@/components/ui/errorMessage";
+import Message from "@/components/ui/Message";
 import Feed from "@/features/feed";
 import FeedLoading from "@/features/feed/FeedLoading";
 import {
@@ -8,78 +8,12 @@ import {
   NyTimesFilters,
   TheGuardianFilters,
 } from "@/features/filters";
-import useApiCall from "@/features/filters/useApiCall";
-import { dateToGuardianFormat, dateToNyTimesFormat } from "@/lib/format-date";
-import { removeFalsyValues } from "@/lib/helpers";
-import { ApiFilters, DataResources } from "@/lib/types";
-import { useQueryClient } from "@tanstack/react-query";
-import { useState } from "react";
-import { FormProvider, useForm } from "react-hook-form";
+import useApiFilterState from "@/features/filters/useApiFilterState";
+import { FormProvider } from "react-hook-form";
 
 export default function Home() {
-  const [selectedOption, setSelectedOption] = useState<{
-    dataSource: DataResources;
-    filters: Record<string, string | number>;
-  }>({
-    dataSource: "NEWS_API",
-    filters: { q: "", category: "general" },
-  });
-
-  const queryClient = useQueryClient();
-
-  const { isLoading, isError, articles, error } = useApiCall(selectedOption);
-  const filtersForm = useForm<ApiFilters>({
-    defaultValues: {
-      ...selectedOption.filters,
-      dataSource: selectedOption.dataSource,
-    },
-  });
-  const { handleSubmit, watch } = filtersForm;
-
-  const onSubmit = handleSubmit(async (data) => {
-    const { dataSource, ...others } = data;
-
-    if (dataSource === "NEWS_API") {
-      const newsApiFilters = removeFalsyValues({
-        q: others.queryNewsApi,
-        category: others.category,
-      });
-      setSelectedOption({
-        dataSource,
-        filters: newsApiFilters,
-      });
-    }
-
-    if (dataSource === "THE_GUARDIAN") {
-      const guardianFilters = removeFalsyValues({
-        q: others.queryGuardian,
-        section: others.sectionGuardian,
-        "from-date": dateToGuardianFormat(others.dateGuardian?.from),
-        "to-date": dateToGuardianFormat(others.dateGuardian?.to),
-      });
-      setSelectedOption({
-        dataSource,
-        filters: guardianFilters,
-      });
-    }
-
-    if (dataSource === "NY_TIMES") {
-      const nyTimesFilter = removeFalsyValues({
-        q: others.queryNyTimes,
-        begin_date: dateToNyTimesFormat(others.dateNyTimes?.from),
-        end_date: dateToNyTimesFormat(others.dateNyTimes?.to),
-        ...(others.sectionNyTimes && {
-          fq: `section_name:(${others.sectionNyTimes})`,
-        }),
-      });
-      setSelectedOption({
-        dataSource,
-        filters: nyTimesFilter,
-      });
-    }
-    await queryClient.invalidateQueries({ queryKey: [dataSource] });
-  });
-
+  const { articles, error, filtersForm, isError, isLoading, onSubmit, watch } =
+    useApiFilterState();
   return (
     <div>
       <FormProvider {...filtersForm}>
@@ -98,7 +32,8 @@ export default function Home() {
         </form>
         {isLoading ? <FeedLoading /> : <Feed articles={articles} />}
         {isError && (
-          <ErrorMessage
+          <Message
+            status="error"
             message={error?.message || "Error fetching articles."}
           />
         )}
