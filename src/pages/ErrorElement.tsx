a7315669a4c24d1b0b75a33c710bb3106d0ed46d
Sepide Kia <sepidekia6990@gmail.com>
2024-12-30 08:53:55 +0330

    fix: change the  errorBoundary to error element

---
+++ src/pages/ErrorElement.tsx
@@ -0,0 +1,24 @@
+import Message from '@/components/ui/Message';
+import { isRouteErrorResponse, useRouteError } from 'react-router';
+
+const ErrorElement = () => {
+  const error = useRouteError();
+  const prod = import.meta.env.PROD;
+
+  return (
+    <div>
+      <main className="prose p-5">
+        <h1>Oops...</h1>
+        {isRouteErrorResponse(error) ? (
+          <Message status="error" message="The requested page was not found." />
+        ) : prod ? (
+          <Message status="error" message="An unexpected error occurred." />
+        ) : (
+          <Message status="error" message={(error as Error).message} />
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default ErrorElement;
